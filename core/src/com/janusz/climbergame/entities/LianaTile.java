a42cc7b6e213cafdd5ff23865e95cd0b007fb67b
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-09-15 22:42:59 +0200

    added 2 more entire lianas (3 total now)

--- core/src/com/janusz/climbergame/entities/LianaTile.java
+++ core/src/com/janusz/climbergame/entities/LianaTile.java
@@ -16,13 +16,15 @@ public class LianaTile extends Image
     public static final int WIDTH = 100;
     public static final int HEIGHT = 300;
 
-    public static final int STARTING_X = 200;
+    public static int STARTING_X = 200;
     public static final int STARTING_Y = Gdx.graphics.getHeight();
 
 
-    public LianaTile()
+    public LianaTile(int starting_x)
     {
         super(new Texture("liana.png"));
+
+        this.STARTING_X = starting_x;
 
         this.setOrigin(WIDTH, HEIGHT);
         this.setSize(WIDTH, HEIGHT);
--- core/src/com/janusz/climbergame/screens/GameScreen.java
+++ core/src/com/janusz/climbergame/screens/GameScreen.java
@@ -15,7 +15,13 @@ import java.util.List;
 public class GameScreen extends AbstractScreen
 {
 
-    public List<LianaTile> lianas;
+    public List<LianaTile> first_wholeLiana;
+    public List<LianaTile> second_wholeLiana;
+    public List<LianaTile> third_wholeLiana;
+
+    private final int first_liana_x = 400;
+    private final int second_liana_x = 700;
+    private final int third_liana_x = 1000;
 
     public GameScreen(ClimberGame game)
     {
@@ -29,7 +35,9 @@ public class GameScreen extends AbstractScreen
 
     private void initLiana()
     {
-        lianas = new ArrayList<LianaTile>();
+        first_wholeLiana = new ArrayList<LianaTile>();
+        second_wholeLiana = new ArrayList<LianaTile>();
+        third_wholeLiana = new ArrayList<LianaTile>();
         createLianaTile();
     }
 
@@ -40,20 +48,15 @@ public class GameScreen extends AbstractScreen
         update();
         moveWholeLianaDown(delta);
 
-        if (lianas.get(lianas.size() - 1).getY() + LianaTile.HEIGHT <= LianaTile.STARTING_Y )
-        {
-            createLianaTile();
-        }
-
-        if (lianas.get(0).getY() + LianaTile.HEIGHT <= 0)
-        {
-            disposeLastLianaTile();
-        }
+        checkIfNeedCreateTile();
+        checkIfNeedDisposeTile();
 
         spriteBatch.begin();
         stage.draw();
         spriteBatch.end();
     }
+
+
 
     private void update()
     {
@@ -62,23 +65,57 @@ public class GameScreen extends AbstractScreen
 
     public void moveWholeLianaDown(float delta)
     {
-        for(LianaTile l : lianas)
+        for (int i = 0 ; i < first_wholeLiana.size() ;i ++)
         {
-            l.moveLianaTile(delta);
+            first_wholeLiana.get(i).moveLianaTile(delta);
+            second_wholeLiana.get(i).moveLianaTile(delta);
+            third_wholeLiana.get(i).moveLianaTile(delta);
+        }
+    }
+
+    private void checkIfNeedCreateTile()
+    {
+        int lastTile = first_wholeLiana.size() - 1;
+
+        if (first_wholeLiana.get(lastTile).getY() + LianaTile.HEIGHT <= LianaTile.STARTING_Y )
+        {
+            createLianaTile();
+        }
+    }
+
+    private void checkIfNeedDisposeTile()
+    {
+        if (first_wholeLiana.get(0).getY() + LianaTile.HEIGHT <= 0)
+        {
+            disposeLastLianaTile();
         }
     }
 
     public void createLianaTile()
     {
-        LianaTile l = new LianaTile();
-        lianas.add(l);
+        LianaTile l = new LianaTile(first_liana_x);
+        first_wholeLiana.add(l);
         stage.addActor(l);
+
+        LianaTile l1 = new LianaTile(second_liana_x);
+        second_wholeLiana.add(l1);
+        stage.addActor(l1);
+
+        LianaTile l2 = new LianaTile(third_liana_x);
+        third_wholeLiana.add(l2);
+        stage.addActor(l2);
     }
 
     public void disposeLastLianaTile()
     {
-        lianas.get(0).remove();
-        lianas.remove(0);
+        first_wholeLiana.get(0).remove();
+        first_wholeLiana.remove(0);
+
+        second_wholeLiana.get(0).remove();
+        second_wholeLiana.remove(0);
+
+        third_wholeLiana.get(0).remove();
+        third_wholeLiana.remove(0);
     }
 
 }
