6195048f0d4669b12a32b93d529c17a434df2c34
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-09-15 22:08:51 +0200

    added LianaTile class

---
+++ core/src/com/janusz/climbergame/entities/LianaTile.java
@@ -0,0 +1,41 @@
+package com.janusz.climbergame.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Janusz on 2017-09-12.
+ */
+
+public class LianaTile extends Image
+{
+    public static final int WIDTH = 100;
+    public static final int HEIGHT = 300;
+
+    public static final int STARTING_X = 200;
+    public static final int STARTING_Y = Gdx.graphics.getHeight();
+
+
+    public LianaTile()
+    {
+        super(new Texture("liana.png"));
+
+        this.setOrigin(WIDTH, HEIGHT);
+        this.setSize(WIDTH, HEIGHT);
+
+        // starting position
+        this.setPosition(STARTING_X, STARTING_Y);
+    }
+
+    public void moveLianaTile(float delta)
+    {
+        this.setY(this.getY() - delta * 250);
+    }
+
+}
+
+
