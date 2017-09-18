872c2cc5c5febbe9f778b024172aebb0586f9d84
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-09-18 12:12:00 +0200

    created Player class with simple default asset

---
+++ core/src/com/janusz/climbergame/entities/Player.java
@@ -0,0 +1,35 @@
+package com.janusz.climbergame.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+/**
+ * Created by Janusz on 2017-09-18.
+ */
+
+public class Player extends Image
+{
+
+    public static final int WIDTH = 100;
+    public static final int HEIGHT = 166;
+
+    public static final int STARTING_X = 750;
+    public static final int STARTING_Y = 100;
+
+    public Player()
+    {
+        super(new Texture("badlogic.jpg"));
+
+        this.setOrigin(0,0);
+        this.setSize(WIDTH,HEIGHT);
+
+        this.setPosition(STARTING_X ,STARTING_Y);
+
+    }
+
+
+
+
+
+}
