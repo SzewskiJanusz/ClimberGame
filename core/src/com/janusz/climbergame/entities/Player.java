da8df34ec37c8ba4d211a1d611d1c53f01a5ef01
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-09-18 15:54:11 +0200

    added „place” field to Player and jump methods

--- core/src/com/janusz/climbergame/entities/Player.java
+++ core/src/com/janusz/climbergame/entities/Player.java
@@ -3,6 +3,7 @@ package com.janusz.climbergame.entities;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.janusz.climbergame.screens.GameScreen;
 
 /**
  * Created by Janusz on 2017-09-18.
@@ -17,6 +18,8 @@ public class Player extends Image
     public static final int STARTING_X = 950;
     public static final int STARTING_Y = 100;
 
+    public int place;
+
     public Player()
     {
         super(new Texture("badlogic.jpg"));
@@ -26,10 +29,23 @@ public class Player extends Image
 
         this.setPosition(STARTING_X ,STARTING_Y);
 
+        place = 2;
     }
 
 
+    public void jumpLeft()
+    {
+        if (place > 0)
+        {
+            place--;
+        }
+    }
 
-
-
+    public void jumpRight()
+    {
+        if (place < 3)
+        {
+            place++;
+        }
+    }
 }
