9741bd31bc16aa5a3bee98ab0a066e62b0509591
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-09-21 09:08:26 +0200

    added random Banana spawning on screen

--- core/src/com/janusz/climbergame/screens/GameScreen.java
+++ core/src/com/janusz/climbergame/screens/GameScreen.java
@@ -3,6 +3,9 @@ package com.janusz.climbergame.screens;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.MathUtils;
+
+import com.badlogic.gdx.utils.Timer;
 import com.janusz.climbergame.ClimberGame;
 import com.janusz.climbergame.entities.Anvil;
 import com.janusz.climbergame.entities.Banana;
@@ -11,25 +14,69 @@ import com.janusz.climbergame.entities.Wall;
 import com.janusz.climbergame.environment.EntireLiana;
 import com.janusz.climbergame.environment.EntireWall;
 
+import java.util.ArrayList;
+import java.util.List;
 
-
-/**
- * Created by Janusz on 2017-08-31.
- */
 
 public class GameScreen extends AbstractScreen
 {
+
 
     private EntireLiana el;
     private EntireWall ew;
 
     public static Player player;
-    private Banana banana;
+
+    private List<Banana> bananas;
+
     private Anvil anvil;
+
+    private float spawnTime;
+
 
     public GameScreen(ClimberGame game)
     {
         super(game);
+
+        randomizeSpawnTime();
+
+        Timer.schedule(new Timer.Task(){
+                           @Override
+                           public void run() {
+                               spawnBanana();
+
+                           }
+                       }
+                , spawnTime       //    (delay)
+                , 1
+        );
+    }
+
+    private void spawnBanana()
+    {
+        int x = selectPlace(MathUtils.random(1,4));
+        Banana b = new Banana(new Texture("banana.png"), x);
+        bananas.add(b);
+        stage.addActor(b);
+    }
+
+    private int selectPlace(int place)
+    {
+        switch (place)
+        {
+            case 1: return Wall.WIDTH;
+            case 2: return EntireLiana.first_liana_x;
+            case 3: return EntireLiana.second_liana_x;
+            case 4: return EntireLiana.third_liana_x;
+            default: return 0;
+        }
+    }
+
+
+
+    private void randomizeSpawnTime()
+    {
+        spawnTime = MathUtils.random(3,7);
     }
 
     protected void init()
@@ -37,9 +84,8 @@ public class GameScreen extends AbstractScreen
         initPlayer();
         el = new EntireLiana();
         ew = new EntireWall();
-        banana = new Banana(new Texture("banana.png"), EntireLiana.first_liana_x);
+        bananas = new ArrayList<Banana>();
         anvil = new Anvil(new Texture("anvil.png"), EntireLiana.second_liana_x);
-        stage.addActor(banana);
         stage.addActor(anvil);
     }
 
@@ -54,24 +100,19 @@ public class GameScreen extends AbstractScreen
     {
         super.render(delta);
         update(delta);
-        checkCollision();
+
 
         spriteBatch.begin();
         stage.draw();
         spriteBatch.end();
     }
 
-    private void checkCollision()
+    private boolean checkCollision(int i)
     {
-        boolean playerEatBanana = Intersector.overlaps(banana.getBounds(), player.getBounds());
+        boolean playerEatBanana = Intersector.overlaps(bananas.get(i).getBounds(), player.getBounds());
 
-        if (playerEatBanana)
-        {
-            banana.remove();
-        }
-
+        return playerEatBanana;
     }
-
 
     private void movePlayer()
     {
@@ -94,9 +135,29 @@ public class GameScreen extends AbstractScreen
         ew.moveWallDown(delta);
         el.moveAllLianasDown(delta);
 
-        banana.update(delta);
+        updateAllBananas(delta);
 
         anvil.update(delta);
+    }
+
+    private void updateAllBananas(float delta)
+    {
+
+        for (int i = 0 ; i < bananas.size() ; i++)
+        {
+            if (checkCollision(i))
+            {
+                bananas.get(i).remove();
+                bananas.remove(i);
+                break;
+            }
+        }
+
+        for(Banana b : bananas)
+        {
+            b.update(delta);
+        }
+
     }
 
 }
