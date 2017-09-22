c194648574b39be1efbf32204d63c2896c9d4230
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-09-22 21:01:27 +0200

    refactored Anvil,by inheriting from AbstractItem

--- core/src/com/janusz/climbergame/entities/Anvil.java
+++ core/src/com/janusz/climbergame/entities/Anvil.java
@@ -10,61 +10,22 @@ import com.janusz.climbergame.ClimberGame;
  * Created by Janusz on 2017-09-20.
  */
 
-public class Anvil extends Image
+public class Anvil extends AbstractItem
 {
 
-    private Rectangle bounds;
-
-    private final int width = 175;
-    private final int height = 125;
-
-    private final int velocity = 500;
-
-    public Anvil(Texture text, int starting_x)
+    public Anvil(Texture text, int starting_x, int starting_y, int width, int height, int velocity)
     {
-        super(text);
-        this.setPosition(starting_x, ClimberGame.HEIGHT);
-        setSize(width,height);
-        setOrigin(getWidth() / 2, getHeight() / 2);
-
-        bounds = new Rectangle(starting_x, ClimberGame.HEIGHT, width, height);
+        super(text, starting_x, starting_y, width, height, velocity);
     }
 
-    public void update(float delta)
-    {
-        checkIfNeedDispose();
-        moveDown(delta);
-        doMovement(delta);
-    }
-
-    @Override
-    public void setY(float y)
-    {
-        this.setPosition(getX(), y);
-        bounds.y = y;
-    }
-
-    private void moveDown(float delta)
+    protected void moveDown(float delta)
     {
         this.setY(this.getY() - velocity * delta);
     }
 
-    private void doMovement(float delta)
+    protected void doMovement(float delta)
     {
         this.rotateBy(20 * delta);
     }
-
-
-    private void checkIfNeedDispose()
-    {
-        if (getY() < 0)
-        {
-            addAction(Actions.removeActor());
-        }
-    }
-
-    public Rectangle getBounds()
-    {
-        return bounds;
-    }
+    
 }
