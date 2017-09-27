0143b541f29ed953e6f3994b0a74cdb91e12fc85
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-09-27 16:20:44 +0200

    created SplashScreen class

---
+++ core/src/com/janusz/climbergame/screens/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.janusz.climbergame.screens;
+
+import com.janusz.climbergame.ClimberGame;
+
+
+/**
+ * Created by Janusz on 2017-09-27.
+ */
+
+public class SplashScreen extends AbstractScreen
+{
+
+    public SplashScreen(ClimberGame game)
+    {
+        super(game);
+
+    }
+
+    @Override
+    protected void init()
+    {
+
+    }
+}
