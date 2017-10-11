49e8600d3e8c6210c94bf30a66efe9ea2c31e3c1
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-10-11 19:00:47 +0200

    added SplashScreen class

--- core/src/com/janusz/climbergame/game/screens/SplashScreen.java
+++ core/src/com/janusz/climbergame/game/screens/SplashScreen.java
@@ -1,6 +1,10 @@
 package com.janusz.climbergame.game.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Timer;
 import com.janusz.climbergame.ClimberGame;
+import com.janusz.climbergame.menu.Title;
+import com.janusz.climbergame.menu.screens.MenuScreen;
 
 
 /**
@@ -10,15 +14,36 @@ import com.janusz.climbergame.ClimberGame;
 public class SplashScreen extends AbstractScreen
 {
 
-    public SplashScreen(ClimberGame game)
+    private Title title;
+
+    public SplashScreen(final ClimberGame game)
     {
         super(game);
 
+        Timer.schedule(new Timer.Task(){
+                           @Override
+                           public void run()
+                           {
+                               // InputProcessor set in MenuScreen constructor because is not
+                               //  initialized now.
+                               game.setScreen(new MenuScreen(game));
+                           }
+                       }
+                , 2
+        );
     }
 
     @Override
     protected void init()
     {
+        title = new Title();
+        stage.addActor(title);
+    }
 
+    @Override
+    public void render(float delta)
+    {
+        super.render(delta);
+        stage.draw();
     }
 }
