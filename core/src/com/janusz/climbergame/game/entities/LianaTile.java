dcbfde449adafce6cba8bed251e6a47ed3c07c31
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-11-03 18:15:20 +0100

    extended LianaTile by AbstractItem

--- core/src/com/janusz/climbergame/game/entities/LianaTile.java
+++ core/src/com/janusz/climbergame/game/entities/LianaTile.java
@@ -9,45 +9,23 @@ import com.janusz.climbergame.ClimberGame;
  * Created by Janusz on 2017-09-12.
  */
 
-public class LianaTile extends Image
+public class LianaTile extends AbstractItem
 {
     public static final int WIDTH = 50;
     public static final int HEIGHT = 120;
 
-    public static int STARTING_X;
+
     public static int STARTING_Y = ClimberGame.HEIGHT;
 
 
     public LianaTile(int starting_x)
     {
-        super(new Texture("liana.png"));
-
-        this.STARTING_X = starting_x;
-
-        this.setOrigin(WIDTH, HEIGHT);
-        this.setSize(WIDTH, HEIGHT);
-
-        // starting position
-        this.setPosition(STARTING_X, STARTING_Y);
+        super(new Texture("liana.png"), starting_x, STARTING_Y, WIDTH , HEIGHT , 250);
     }
 
     public LianaTile(int starting_x,int starting_y)
     {
-        super(new Texture("liana.png"));
-
-        this.STARTING_X = starting_x;
-        this.STARTING_Y = starting_y;
-
-        this.setOrigin(WIDTH, HEIGHT);
-        this.setSize(WIDTH, HEIGHT);
-
-        // starting position
-        this.setPosition(STARTING_X, STARTING_Y);
-    }
-
-    public void moveLianaTile(float delta)
-    {
-        this.setY(this.getY() - delta * 250);
+        super(new Texture("liana.png"), starting_x, starting_y, WIDTH , HEIGHT , 250);
     }
 
     public void stickLianaToAnother(LianaTile destination)
@@ -55,6 +33,17 @@ public class LianaTile extends Image
         this.setY(destination.getY() + HEIGHT);
     }
 
+    @Override
+    public void moveDown(float delta)
+    {
+        this.setY(this.getY() - delta * 250);
+    }
+
+    @Override
+    protected void doMovement(float delta)
+    {
+
+    }
 }
 
 
