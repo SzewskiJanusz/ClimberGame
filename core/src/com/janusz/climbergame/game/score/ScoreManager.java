5b85c616b76641a5724e7a130f4420a8940f5436
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-11-28 23:21:47 +0100

    made singleton in ScoreManager class

--- core/src/com/janusz/climbergame/game/score/ScoreManager.java
+++ core/src/com/janusz/climbergame/game/score/ScoreManager.java
@@ -3,6 +3,7 @@ package com.janusz.climbergame.game.score;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.janusz.climbergame.game.managers.energy.EnergyManager;
 
 /**
  * Created by Janusz on 2017-11-28.
@@ -13,7 +14,18 @@ public class ScoreManager
     public ScoreLabel ScoreLabel;
     public ScoreLogic ScoreLogic;
 
-    public ScoreManager()
+    // Singleton
+    private static ScoreManager scoreManager;
+
+    public static ScoreManager getInstance()
+    {
+        if(scoreManager == null) {
+            scoreManager = new ScoreManager();
+        }
+        return scoreManager;
+    }
+    //
+    private ScoreManager()
     {
         ScoreLogic = new ScoreLogic();
         ScoreLabel = new ScoreLabel(String.valueOf(ScoreLogic.getScore()), getDefaultLabelStyle());
@@ -25,5 +37,15 @@ public class ScoreManager
         ls.font = new BitmapFont();
         ls.fontColor = Color.WHITE;
         return ls;
+    }
+
+    public void update()
+    {
+        passPointsToLabel();
+    }
+
+    private void passPointsToLabel()
+    {
+        ScoreLabel.setPoints(ScoreLogic.getScore());
     }
 }
