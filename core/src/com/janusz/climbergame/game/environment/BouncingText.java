034e29e39c2e5e54f9d5ca520ce1a810421255e7
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-11-30 11:35:02 +0100

    created BouncingText class

---
+++ core/src/com/janusz/climbergame/game/environment/BouncingText.java
@@ -0,0 +1,58 @@
+package com.janusz.climbergame.game.environment;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.janusz.climbergame.game.entities.player.Player;
+
+/**
+ * Created by Janusz on 2017-11-30.
+ */
+
+public class BouncingText extends Label
+{
+    // Time for when text is visible (sec)
+    private final float timeOfVisibility = 0.5f;
+    private float timeElapsed = 0;
+
+    public BouncingText(CharSequence text, LabelStyle style, Effect effect)
+    {
+        super(text, style);
+
+        setFontScale(1);
+        this.setX(Player.instance().getX());
+        this.setY(Player.instance().getY());
+
+        switch(effect)
+        {
+            case GOOD:
+                this.setColor(Color.GREEN);
+                break;
+            case DEFAULT:
+                this.setColor(Color.WHITE);
+                break;
+            case BAD:
+                this.setColor(Color.RED);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void act(float delta)
+    {
+        super.act(delta);
+        timeElapsed += delta;
+        if (timeOfVisibility < timeElapsed)
+        {
+            this.remove();
+            timeElapsed = 0;
+
+        }
+        else
+        {
+            moveBy(0,5);
+        }
+    }
+}
