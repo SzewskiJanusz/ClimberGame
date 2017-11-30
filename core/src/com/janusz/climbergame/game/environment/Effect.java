5bc44ee3d407f668ef833d2b88e7de7ebefb4dbf
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-11-30 11:35:25 +0100

    created enum for determine bouncingText color

---
+++ core/src/com/janusz/climbergame/game/environment/Effect.java
@@ -0,0 +1,15 @@
+package com.janusz.climbergame.game.environment;
+
+/**
+ * Created by Janusz on 2017-11-30.
+ */
+
+/**
+ * Represent effects given by entities. For example banana refills energy then its good.
+ * Tequilla makes u drunk so its bad.
+ * Money gives points and its default
+ */
+public enum Effect
+{
+    GOOD,DEFAULT,BAD
+}
