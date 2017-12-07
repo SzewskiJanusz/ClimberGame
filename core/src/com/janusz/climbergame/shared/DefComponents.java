4d9dc30848d53d76cf6daa76791007e1a7ef2e1c
Aleksander Januszewski <alex.januszewski@interia.pl>
2017-12-07 11:42:22 +0100

    added style initialization in DefComponents

--- core/src/com/janusz/climbergame/shared/DefComponents.java
+++ core/src/com/janusz/climbergame/shared/DefComponents.java
@@ -1,14 +1,20 @@
 package com.janusz.climbergame.shared;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 /**
  * Created by Janusz on 2017-11-30.
  */
 
-public abstract class DefLabel
+public abstract class DefComponents
 {
     public static Label.LabelStyle getDefaultLabelStyle()
     {
@@ -16,5 +22,25 @@ public abstract class DefLabel
         ls.font = new BitmapFont();
         ls.fontColor = Color.WHITE;
         return ls;
+    }
+
+    public static TextField.TextFieldStyle getDefaultTextfieldStyle()
+    {
+        TextField.TextFieldStyle tStyle = new TextField.TextFieldStyle();
+        tStyle.font = new BitmapFont();
+        tStyle.fontColor = Color.WHITE;
+        return tStyle;
+    }
+
+    public static TextButton.TextButtonStyle getTextButtonStyle()
+    {
+        TextButton.TextButtonStyle bs;
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("ui-green.atlas"));
+        Skin skin = new Skin(atlas);
+        bs = new TextButton.TextButtonStyle();
+        bs.font = new BitmapFont();
+        bs.up = skin.getDrawable("button_02");
+        bs.down = skin.getDrawable("button_06");
+        return bs;
     }
 }
