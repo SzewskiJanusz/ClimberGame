6c171cedee591229e773708fa6f2f5521b98fe97
Aleksander Januszewski <alex.januszewski@interia.pl>
2018-02-09 12:48:18 +0100

    removed good and bad managers

--- core/src/com/janusz/climbergame/game/managers/AbstractManager.java
+++ core/src/com/janusz/climbergame/game/managers/AbstractManager.java
@@ -1,8 +1,8 @@
 package com.janusz.climbergame.game.managers;
 
 import com.badlogic.gdx.utils.Timer;
+import com.janusz.climbergame.game.entities.AbstractItem;
 import com.janusz.climbergame.game.environment.EntireLiana;
-import com.janusz.climbergame.game.screens.GameScreen;
 
 /**
  * Created by Janusz on 2018-02-03.
@@ -10,13 +10,10 @@ import com.janusz.climbergame.game.screens.GameScreen;
 
 abstract class AbstractManager
 {
-    // Lista obiektów
+    // Time after spawn occurs
     protected float entitySpawnTime;
+    // Spawn delay
     private int delay ;
-
-    /*
-     * Metody abstrakacyjne do poprawnego zarządzania obiektami
-     */
 
     /**
      * Losuje liczbę entitySpawnTime
@@ -26,12 +23,7 @@ abstract class AbstractManager
     /**
      * Tworzy ustalony obiekt
      */
-    protected abstract void spawnEntity();
-
-    /**
-     * Wywołanie efektu po kolizji
-     */
-    protected abstract void triggerEffect();
+    protected abstract void createEntity();
 
     /**
      * Initialize List and timer
@@ -42,21 +34,6 @@ abstract class AbstractManager
         this.delay = delay;
         randomizeSpawnTime();
     }
-
-    /**
-     * Odświeża wszystkie obiekty menadżera
-     * @param delta - czas w jednej klatce
-     */
-    abstract public void updateEntities(float delta);
-
-
-    /**
-     * Sprawdza czy obiekt ma kolizje z graczem
-     * @param i - indeks poszczególnego obiektu
-     * @return
-     */
-    abstract protected boolean checkCollision(int i);
-
 
     /**
      * Zwraca X danego miejsca w widoku
@@ -80,8 +57,7 @@ abstract class AbstractManager
                            @Override
                            public void run()
                            {
-                               if (!GameScreen.paused)
-                                    spawnEntity();
+                               createEntity();
                            }
                        }
                 , delay       //    (delay)
---
+++ core/src/com/janusz/climbergame/game/managers/queue/QueueManager.java
@@ -0,0 +1,9 @@
+package com.janusz.climbergame.game.managers.queue;
+
+/**
+ * Created by Janusz on 2018-02-09.
+ */
+
+public class QueueManager
+{
+}
