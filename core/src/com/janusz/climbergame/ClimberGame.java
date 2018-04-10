2b0c0b1fb8728194e7fe5e34892a4d97ffeeb2bd
Aleksander Januszewski <alex.januszewski@interia.pl>
2018-04-10 12:11:58 +0200

    moved all memory releasing methods to one dispose() method in ClimberGame

--- core/src/com/janusz/climbergame/ClimberGame.java
+++ core/src/com/janusz/climbergame/ClimberGame.java
@@ -2,7 +2,13 @@ package com.janusz.climbergame;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.janusz.climbergame.game.entities.player.Player;
+import com.janusz.climbergame.game.environment.EntireLiana;
+import com.janusz.climbergame.game.indicators.graphics.IndicatorController;
+import com.janusz.climbergame.game.managers.queue.QueueManager;
 import com.janusz.climbergame.game.pause.PauseController;
+import com.janusz.climbergame.game.sound.GameSound;
+import com.janusz.climbergame.game.texts.TapImage;
 import com.janusz.climbergame.menu.screens.MenuScreen;
 import com.janusz.climbergame.shared.DefComponents;
 
@@ -48,7 +54,14 @@ public class ClimberGame extends Game implements AdService
 	public void dispose()
 	{
 		super.dispose();
-		PauseController.instance().dispose();
+		TapImage.ins = null;
+		Player.player = null;
+		PauseController.dispose();
+		GameSound.dispose();
+		QueueManager.dispose();
+		IndicatorController.indControl = null;
+		EntireLiana.dispose();
+
 	}
 
 	@Override
