a840b98631bb4dec60cb3c793707d25a292d67d3
Aleksander Januszewski <alex.januszewski@interia.pl>
2018-04-10 11:19:15 +0200

    implemented ads in AndroidLauncher and added constructor and implement interface in ClimberGame

--- android/src/com/janusz/climbergame/AndroidLauncher.java
+++ android/src/com/janusz/climbergame/AndroidLauncher.java
@@ -1,18 +1,92 @@
 package com.janusz.climbergame;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.janusz.climbergame.ClimberGame;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
-public class AndroidLauncher extends AndroidApplication {
+
+public class AndroidLauncher extends AndroidApplication implements AdService{
+
+
+    private InterstitialAd mInterstitialAd;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		final AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
 		config.useCompass = false;
-		initialize(new ClimberGame(), config);
+
+        MobileAds.initialize(this, "ca-app-pub-9491149384478370~6013353900");
+
+		RelativeLayout layout = new RelativeLayout(this);
+
+		View gameView = initializeForView(new ClimberGame(this), config);
+		AdView adView = new AdView(this);
+		adView.setAdSize(AdSize.SMART_BANNER);
+		adView.setVisibility(View.VISIBLE);
+		adView.setAdUnitId("ca-app-pub-9491149384478370/7797564085");
+		AdRequest adRequest = new AdRequest.Builder().build();
+		adView.loadAd(adRequest);
+
+		RelativeLayout.LayoutParams adParams =
+				new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+						RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+
+
+		layout.addView(gameView);
+
+		layout.addView(adView, adParams);
+		setContentView(layout);
+        mInterstitialAd = new InterstitialAd(this);
+		//ca-app-pub-9491149384478370/7828142227
+        mInterstitialAd.setAdUnitId("ca-app-pub-9491149384478370/7828142227");
 	}
+
+    @Override
+    public boolean isInterstitialLoaded()
+    {
+        return mInterstitialAd.isLoaded();
+    }
+
+    @Override
+    public void showInterstitial()
+    {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                if (mInterstitialAd.isLoaded())
+                    mInterstitialAd.show();
+            }
+        });
+    }
+
+	@Override
+    public void loadIntersitialAd(){
+		runOnUiThread(new Runnable() {
+			public void run() {
+				if(!isInterstitialLoaded())
+				{
+					AdRequest interstitialRequest = new AdRequest.Builder().build();
+					mInterstitialAd.loadAd(interstitialRequest);
+				}
+			}
+		});
+    }
 }
--- core/src/com/janusz/climbergame/ClimberGame.java
+++ core/src/com/janusz/climbergame/ClimberGame.java
@@ -3,23 +3,29 @@ package com.janusz.climbergame;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.janusz.climbergame.game.pause.PauseController;
-import com.janusz.climbergame.game.screens.SaveScoreScreen;
-import com.janusz.climbergame.game.screens.SplashScreen;
 import com.janusz.climbergame.menu.screens.MenuScreen;
 import com.janusz.climbergame.shared.DefComponents;
 
-public class ClimberGame extends Game {
+public class ClimberGame extends Game implements AdService
+{
 
 	public static int WIDTH = 700;
 	public static int HEIGHT = 480;
 
 	private boolean paused;
 
+	public AdService adService;
+
+	public ClimberGame(AdService ads){
+		adService=ads;
+	}
+
 	@Override
 	public void create()
 	{
 		DefComponents.createFonts();
         DefComponents.prepareStyles();
+		Gdx.input.setCatchBackKey(true);
 		setScreen(new MenuScreen(this));
 	}
 
@@ -44,5 +50,20 @@ public class ClimberGame extends Game {
 		super.dispose();
 		PauseController.instance().dispose();
 	}
+
+	@Override
+	public boolean isInterstitialLoaded()
+	{
+		return false;
+	}
+
+	@Override
+	public void showInterstitial()
+	{}
+
+	@Override
+	public void loadIntersitialAd()
+	{}
+
 }
 
