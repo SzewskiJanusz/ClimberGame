891dc667af6597dbe42181f5bb97dbcb96a5753c
Aleksander Januszewski <alex.januszewski@interia.pl>
2018-04-10 11:16:39 +0200

    added AdService interface to implement ads in AndroidLauncher

---
+++ core/src/com/janusz/climbergame/AdService.java
@@ -0,0 +1,16 @@
+package com.janusz.climbergame;
+
+/**
+ * Created by Janusz on 2018-03-31.
+ */
+
+/**
+ * Created by Janusz on 2018-03-31.
+ */
+
+public interface AdService {
+
+    boolean isInterstitialLoaded();
+    void showInterstitial();
+    void loadIntersitialAd();
+}
