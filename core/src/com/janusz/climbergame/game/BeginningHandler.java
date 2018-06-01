0310620c84358d0128dc26451e79364be56c1f0c
Aleksander Januszewski <alex.januszewski@interia.pl>
2018-06-01 08:00:47 +0200

    shuffle spawn time items every new game

--- core/src/com/janusz/climbergame/game/BeginningHandler.java
+++ core/src/com/janusz/climbergame/game/BeginningHandler.java
@@ -2,6 +2,7 @@ package com.janusz.climbergame.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.math.MathUtils;
 import com.janusz.climbergame.game.entities.player.Player;
 import com.janusz.climbergame.game.managers.score.ScoreManager;
 import com.janusz.climbergame.game.screens.GameScreen;
@@ -26,15 +27,26 @@ public class BeginningHandler extends InputAdapter
     {
         Gdx.input.setInputProcessor(GameScreen.stage);
         GameScreen.onBeginning = false;
+        int firstTimer = MathUtils.random(1,6);
 
-        screen.bananaMgr.startTimer();
-        screen.anvilMgr.startTimer();
-        screen.stoneMgr.startTimer();
-        screen.coffeeMgr.startTimer();
-        screen.tequilaMgr.startTimer();
-        screen.appleMgr.startTimer();
+        switch (firstTimer)
+        {
+            case 1: screen.bananaMgr.startTimer(3); break;
+            case 2: screen.anvilMgr.startTimer(3); break;
+            case 3: screen.coffeeMgr.startTimer(3); break;
+            case 4: screen.tequilaMgr.startTimer(3); break;
+            case 5: screen.appleMgr.startTimer(3); break;
+            case 6: screen.stoneMgr.startTimer(3); break;
+        }
+        if (firstTimer != 1) screen.bananaMgr.startTimer(MathUtils.random(4,10));
+        if (firstTimer != 2) screen.anvilMgr.startTimer(MathUtils.random(4,10));
+        if (firstTimer != 6) screen.stoneMgr.startTimer(MathUtils.random(4,10));
+        if (firstTimer != 3) screen.coffeeMgr.startTimer(MathUtils.random(4,10));
+        if (firstTimer != 4) screen.tequilaMgr.startTimer(MathUtils.random(4,10));
+        if (firstTimer != 5) screen.appleMgr.startTimer(MathUtils.random(4,10));
 
-        GameScreen.stage.addActor(screen.lvlLabel);
+        GameScreen.stage.addActor(screen.lvlTexts.levelNumber);
+        GameScreen.stage.addActor(screen.lvlTexts.levelText);
         TapToStartLabel.instance().remove();
         TapImage.instance().remove();
         GameScreen.stage.addActor(ScoreManager.getInstance().ScoreLabel);
