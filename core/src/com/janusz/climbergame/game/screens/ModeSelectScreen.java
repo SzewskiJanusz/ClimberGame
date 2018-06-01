c2bde40e8ba8ad4c68a1dde057659cd69cc0a9f2
Aleksander Januszewski <alex.januszewski@interia.pl>
2018-06-01 07:59:55 +0200

    new screen with normal and hardcore modes

---
+++ core/src/com/janusz/climbergame/game/screens/ModeSelectScreen.java
@@ -0,0 +1,111 @@
+package com.janusz.climbergame.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.janusz.climbergame.ClimberGame;
+import com.janusz.climbergame.game.BeginningHandler;
+import com.janusz.climbergame.game.background.JungleBackground;
+import com.janusz.climbergame.menu.Title;
+import com.janusz.climbergame.menu.buttons.ButtonFactory;
+import com.janusz.climbergame.menu.screens.MenuScreen;
+import com.janusz.climbergame.shared.AbstractScreen;
+import com.janusz.climbergame.shared.DefComponents;
+
+/**
+ * Created by Janusz on 2018-05-26.
+ */
+
+public class ModeSelectScreen extends AbstractScreen
+{
+    private Title title;
+    private JungleBackground gameBackground;
+
+    public ModeSelectScreen(ClimberGame game)
+    {
+        super(game);
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    protected void init()
+    {
+        initBackground();
+        initTitle();
+        initButtons();
+    }
+
+    private void initBackground()
+    {
+        gameBackground = new JungleBackground();
+        stage.addActor(gameBackground);
+    }
+
+    private void initTitle()
+    {
+        title = new Title();
+        stage.addActor(title);
+    }
+
+    private void initButtons()
+    {
+        initNormalButton();
+        initHardcoreButton();
+        initBackButton();
+    }
+
+    private void initNormalButton()
+    {
+        TextButton normal = ButtonFactory.createButton
+                ("NORMAL", DefComponents.TEXTBUTTON_STYLE, 320);
+        normal.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                GameScreen gs = new GameScreen(game);
+                Gdx.input.setInputProcessor(new BeginningHandler(gs));
+                game.setScreen(gs);
+            }
+        });
+        stage.addActor(normal);
+    }
+
+    private void initHardcoreButton()
+    {
+        TextButton hardcore = ButtonFactory.createButton
+                ("HARDCORE", DefComponents.TEXTBUTTON_STYLE, 220);
+        hardcore.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+//                GameScreen gs = new GameScreen(game);
+//                Gdx.input.setInputProcessor(new BeginningHandler(gs));
+//                game.setScreen(gs);
+            }
+        });
+        stage.addActor(hardcore);
+    }
+
+    private void initBackButton()
+    {
+        TextButton back = ButtonFactory.createButton("BACK", DefComponents.TEXTBUTTON_STYLE, 40);
+        back.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                game.setScreen(new MenuScreen(game));
+            }
+        });
+        stage.addActor(back);
+    }
+
+    @Override
+    public void render(float delta)
+    {
+        super.render(delta);
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+}
