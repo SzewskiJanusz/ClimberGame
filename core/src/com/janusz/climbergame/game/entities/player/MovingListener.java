c5a59effb4667ab8e10e64bda82f74340e22c078
Aleksander Januszewski <alex.januszewski@interia.pl>
2018-08-06 08:41:10 +0200

    added MovingListener for Player

---
+++ core/src/com/janusz/climbergame/game/entities/player/MovingListener.java
@@ -0,0 +1,52 @@
+package com.janusz.climbergame.game.entities.player;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.janusz.climbergame.game.managers.pause.PauseController;
+
+public class MovingListener extends InputListener
+{
+    Player player;
+    PauseController pauseController;
+
+    public MovingListener(Player player, PauseController pauseC)
+    {
+        this.player = player;
+        this.pauseController = pauseC;
+    }
+
+    @Override
+    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
+    {
+        if (pauseController.pauseClicked)
+        {
+            pauseController.pauseClicked = false;
+            return false;
+        }
+
+        if (player.playerState == PlayerState.CLIMBING_LIANA && !pauseController.isGamePaused())
+        {
+            if (isFingerOnLeft(x, player.getX() + player.getWidth() / 2))
+            {
+                if (!Player.drunk)
+                    player.jumpLeft();
+                else
+                    player.jumpRight();
+            }
+            else
+            {
+                if (!Player.drunk)
+                    player.jumpRight();
+                else
+                    player.jumpLeft();
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isFingerOnLeft(float screenX, float playerOnScreenX)
+    {
+        return playerOnScreenX > screenX;
+    }
+}
